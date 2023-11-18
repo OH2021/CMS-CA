3e094e0bbe1f2e7b138664e6ac671e8e90b2db3c
OH2021 <77490624+OH2021@users.noreply.github.com>
2023-11-18 16:43:42 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -37,3 +37,36 @@ function displayProductDetails(productDetails) {
     document.getElementById('productDescription').textContent = productDetails.description;
     document.getElementById('productPrice').textContent = productDetails.price;
 }
+
+document.addEventListener('DOMContentLoaded', async () => {
+    const data = await fetchData();
+    addDataToWebsite(data);
+  });
+
+// API
+document.addEventListener('DOMContentLoaded', () => {
+    // Fetch product details from the WordPress API
+    fetch('https://fl-power.no/exam/wp-json/wc/v3/products?consumer_key=ck_3ff46144f22e06ae275285802b5b282129a841f2&consumer_secret=cs_ae3ebd41e2a7ef6caba586991663ac0e05bf6c2a')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(products => {
+            const product = products[0];
+
+            const productImage = document.getElementById('productImage');
+            const productName = document.getElementById('productName');
+            const productDescription = document.getElementById('productDescription');
+            const productPrice = document.getElementById('productPrice');
+
+            const imageUrl = product.images && product.images.length > 0 ? product.images[0].src : '';
+
+            productImage.src = imageUrl || '';
+            productName.textContent = product.name || 'Product Name';
+            productDescription.innerHTML = product.description || 'Product Description. Lorem ipsum dolor sit amet, consectetur adipiscing elit.';
+            productPrice.textContent = `$${product.price || '0.00'}`;
+        })
+        .catch(error => console.error('Error fetching product details:', error));
+});
