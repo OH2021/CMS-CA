b3e6515d5c68807ca1d1c9c3596a176a0271312d
OH2021 <77490624+OH2021@users.noreply.github.com>
2023-11-19 17:06:04 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,4 +1,40 @@
-document.addEventListener('DOMContentLoaded', function () {
+fetch('https://fl-power.no/exam/wp-json/wc/v3/products?consumer_key=ck_3ff46144f22e06ae275285802b5b282129a841f2&consumer_secret=cs_ae3ebd41e2a7ef6caba586991663ac0e05bf6c2a')
+    .then(response => response.json())
+    .then(products => {
+        const productContainer = document.querySelector('.product-list');
+
+if (productContainer) {
+    // Your existing code to append products
+}
+        products.forEach(product => {
+            const productItem = document.createElement('div');
+            productItem.classList.add('product-item');
+            productItem.setAttribute('data-product-id', product.id);
+
+            const h2 = document.createElement('h2');
+            h2.textContent = product.name;
+
+            const p = document.createElement('p');
+            p.textContent = product.description;
+
+            const span = document.createElement('span');
+            span.textContent = `$${product.price}`;
+
+            const a = document.createElement('a');
+            a.href = `single-product.html?id=${product.id}&name=${encodeURIComponent(product.name)}&price=${product.price}`;
+            a.classList.add('view-details-btn');
+            a.textContent = 'View Details';
+
+            productItem.appendChild(h2);
+            productItem.appendChild(span);
+            productItem.appendChild(a);
+
+            productContainer.appendChild(productItem);
+        });
+    })
+    .catch(error => console.error('Error fetching data:', error));
+
+    document.addEventListener('DOMContentLoaded', function () {
     const url = new URL(window.location.href);
     const productId = url.searchParams.get('id');
 
@@ -32,44 +68,5 @@ document.addEventListener('DOMContentLoaded', function () {
             .catch(error => console.error('Error fetching product details:', error));
     } else {
         console.error('Error: Product ID not found in the URL.');
-
-        // Add any additional logic for handling the absence of a product ID if needed
     }
-
-    fetch('https://fl-power.no/exam/wp-json/wc/v3/products?consumer_key=ck_3ff46144f22e06ae275285802b5b282129a841f2&consumer_secret=cs_ae3ebd41e2a7ef6caba586991663ac0e05bf6c2a')
-        .then(response => response.json())
-        .then(products => {
-            const productContainer = document.querySelector('.product-list');
-
-            if (productContainer) {
-                products.forEach(product => {
-                    const productItem = document.createElement('div');
-                    productItem.classList.add('product-item');
-                    productItem.setAttribute('data-product-id', product.id);
-
-                    const h2 = document.createElement('h2');
-                    h2.textContent = product.name;
-
-                    const p = document.createElement('p');
-                    p.textContent = product.description;
-
-                    const span = document.createElement('span');
-                    span.textContent = `$${product.price}`;
-
-                    const a = document.createElement('a');
-                    a.href = `single-product.html?id=${product.id}&name=${encodeURIComponent(product.name)}&price=${product.price}`;
-                    a.classList.add('view-details-btn');
-                    a.textContent = 'View Details';
-
-                    productItem.appendChild(h2);
-                    productItem.appendChild(span);
-                    productItem.appendChild(a);
-
-                    productContainer.appendChild(productItem);
-                });
-            } else {
-                console.error('Error: Product container element not found.');
-            }
-        })
-        .catch(error => console.error('Error fetching data:', error));
 });
