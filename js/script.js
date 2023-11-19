98153265bb88935f21f42e37d5b916459d5d3869
OH2021 <77490624+OH2021@users.noreply.github.com>
2023-11-19 13:06:38 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -8,65 +8,53 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 });
-
-document.addEventListener('DOMContentLoaded', function () {
-    const urlParams = new URLSearchParams(window.location.search);
-    const productId = urlParams.get('id');
-
-    fetchProductDetails(productId);
-});
-
-function fetchProductDetails(productId) {
-    const productDetails = getDummyProductDetails(productId);
-
-    displayProductDetails(productDetails);
-}
-
-function getDummyProductDetails(productId) {
-    return {
-        name: `Product ${productId}`,
-        description: `Description of Product ${productId}. Lorem ipsum dolor sit amet, consectetur adipiscing elit.`,
-        price: `$${(productId * 10).toFixed(2)}`,
-        image: `product${productId}.jpg` 
-    };
-}
-
-function displayProductDetails(productDetails) {
-    document.getElementById('productImage').src = productDetails.image;
-    document.getElementById('productName').textContent = productDetails.name;
-    document.getElementById('productDescription').textContent = productDetails.description;
-    document.getElementById('productPrice').textContent = productDetails.price;
-}
-
-document.addEventListener('DOMContentLoaded', async () => {
-    const data = await fetchData();
-    addDataToWebsite(data);
-  });
-
 // API
-document.addEventListener('DOMContentLoaded', () => {
-    // Fetch product details from the WordPress API
-    fetch('https://fl-power.no/exam/wp-json/wc/v3/products?consumer_key=ck_3ff46144f22e06ae275285802b5b282129a841f2&consumer_secret=cs_ae3ebd41e2a7ef6caba586991663ac0e05bf6c2a')
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(products => {
-            const product = products[0];
+fetch('https://fl-power.no/exam/wp-json/wc/v3/products?consumer_key=ck_3ff46144f22e06ae275285802b5b282129a841f2&consumer_secret=cs_ae3ebd41e2a7ef6caba586991663ac0e05bf6c2a')
+    .then(response => response.json())
+    .then(products => {
+        const productContainer = document.querySelector('.product-list');
+        products.forEach(product => {
+            const productItem = document.createElement('div');
+            productItem.classList.add('product-item');
+            productItem.setAttribute('data-product-id', product.id);
 
-            const productImage = document.getElementById('productImage');
-            const productName = document.getElementById('productName');
-            const productDescription = document.getElementById('productDescription');
-            const productPrice = document.getElementById('productPrice');
+            const h2 = document.createElement('h2');
+            h2.textContent = product.name;
 
-            const imageUrl = product.images && product.images.length > 0 ? product.images[0].src : '';
+            const p = document.createElement('p');
+            p.textContent = product.description;
 
-            productImage.src = imageUrl || '';
-            productName.textContent = product.name || 'Product Name';
-            productDescription.innerHTML = product.description || 'Product Description. Lorem ipsum dolor sit amet, consectetur adipiscing elit.';
-            productPrice.textContent = `$${product.price || '0.00'}`;
-        })
-        .catch(error => console.error('Error fetching product details:', error));
-});
+            const span = document.createElement('span');
+            span.textContent = `$${product.price}`;
+
+            const a = document.createElement('a');
+            a.href = `single-product.html?id=${product.id}&name=${encodeURIComponent(product.name)}&price=${product.price}`;
+            a.classList.add('view-details-btn');
+            a.textContent = 'View Details';
+
+            productItem.appendChild(h2);
+            productItem.appendChild(p);
+            productItem.appendChild(span);
+            productItem.appendChild(a);
+
+            productContainer.appendChild(productItem);
+        });
+    })
+    .catch(error => console.error('Error fetching data:', error));
+
+    document.addEventListener("DOMContentLoaded", function () {
+        // Get the product ID from the query parameters
+        const urlParams = new URLSearchParams(window.location.search);
+        const productId = urlParams.get('id');
+    
+        // Fetch data for the specific product using the product ID
+        fetch(`https://fl-power.no/exam/wp-json/wc/v3/products/${productId}?consumer_key=ck_3ff46144f22e06ae275285802b5b282129a841f2&consumer_secret=cs_ae3ebd41e2a7ef6caba586991663ac0e05bf6c2a`)
+            .then(response => response.json())
+            .then(product => {
+                // Update the product details on the page
+                document.getElementById('productName').textContent = product.name;
+                document.getElementById('productDescription').textContent = product.description;
+                document.getElementById('productPrice').textContent = `$${product.price}`;
+            })
+            .catch(error => console.error('Error fetching product details:', error));
+    });
