12c20be0901b3e52f0cf6c2b08cbf80b804f12fb
OH2021 <77490624+OH2021@users.noreply.github.com>
2023-11-30 12:32:12 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,11 +1,40 @@
-fetch('https://fl-power.no/exam/wp-json/wc/v3/products?consumer_key=ck_3ff46144f22e06ae275285802b5b282129a841f2&consumer_secret=cs_ae3ebd41e2a7ef6caba586991663ac0e05bf6c2a')
-    .then(response => response.json())
-    .then(products => {
-        const productContainer = document.querySelector('.product-list');
-
-if (productContainer) {
-    // Your existing code to append products
+function fetchAllProducts() {
+    return fetch('https://fl-power.no/exam/wp-json/wc/v3/products?consumer_key=ck_3ff46144f22e06ae275285802b5b282129a841f2&consumer_secret=cs_ae3ebd41e2a7ef6caba586991663ac0e05bf6c2a')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Failed to fetch products');
+            }
+            return response.json();
+        });
 }
+
+function fetchSingleProduct(productId) {
+    return fetch(`https://fl-power.no/exam/wp-json/wc/v3/products/${productId}?consumer_key=ck_3ff46144f22e06ae275285802b5b282129a841f2&consumer_secret=cs_ae3ebd41e2a7ef6caba586991663ac0e05bf6c2a`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Product not found');
+            }
+            return response.json();
+        });
+}
+
+function updateProductDetails(product) {
+
+    const productNameElement = document.getElementById('productName');
+    const productPriceElement = document.getElementById('productPrice');
+
+    if (productNameElement && productPriceElement) {
+        productNameElement.textContent = product.name;
+        productPriceElement.textContent = `$${product.price}`;
+    } else {
+        console.error('Error: Product details elements not found.');
+    }
+}
+
+function displayProducts(products) {
+    const productContainer = document.querySelector('.product-list');
+
+    if (productContainer) {
         products.forEach(product => {
             const productItem = document.createElement('div');
             productItem.classList.add('product-item');
@@ -31,42 +60,28 @@ if (productContainer) {
 
             productContainer.appendChild(productItem);
         });
-    })
-    .catch(error => console.error('Error fetching data:', error));
+    }
+}
 
-    document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function () {
     const url = new URL(window.location.href);
     const productId = url.searchParams.get('id');
 
     console.log('Product ID from URL:', productId);
 
     if (productId) {
-        const updateProductDetails = (product) => {
-            console.log('Updating product details:', product);
-
-            const productNameElement = document.getElementById('productName');
-            const productPriceElement = document.getElementById('productPrice');
-
-            if (productNameElement && productPriceElement) {
-                productNameElement.textContent = product.name;
-                productPriceElement.textContent = `$${product.price}`;
-            } else {
-                console.error('Error: Product details elements not found.');
-            }
-        };
-
-        fetch(`https://fl-power.no/exam/wp-json/wc/v3/products/${productId}?consumer_key=ck_3ff46144f22e06ae275285802b5b282129a841f2&consumer_secret=cs_ae3ebd41e2a7ef6caba586991663ac0e05bf6c2a`)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Product not found');
-                }
-                return response.json();
-            })
+        fetchSingleProduct(productId)
             .then(product => {
                 updateProductDetails(product);
             })
             .catch(error => console.error('Error fetching product details:', error));
     } else {
-        console.error('Error: Product ID not found in the URL.');
+        console.warn('No Product ID in the URL. Proceeding to fetch all products.');
+
+        fetchAllProducts()
+            .then(products => {
+                displayProducts(products);
+            })
+            .catch(error => console.error('Error fetching data:', error));
     }
 });
